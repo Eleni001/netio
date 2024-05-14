8d8318230aabbfc9cc3683f4e791fcc77a093fc9
Tomoyo Alvåg <tomoyo.tomoyo.m@gmail.com>
2024-05-14 14:43:51 +0200

    deleted test function

--- app/app.ts
+++ app/app.ts
@@ -2,12 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function main () {
- await db.user.create({
+async function main() {
+  await db.user.create({
     data: {
-        email: "123@mail.com"
-    }
-    
- })
+      email: "123@mail.com",
+    },
+  });
 }
-main()
+main();
