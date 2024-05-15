2089f745acaf8aef9d9e514e8b9939eae2f08a75
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-05-15 22:48:52 +0200

    install ts-node and add seed to create user

--- prisma/seedScripts/user.ts
+++ prisma/seedScripts/user.ts
@@ -1,1 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
 // export const userSeed = async () => {};
+
+export async function createUser(db: PrismaClient) {
+    const Peter = await db.user.upsert({
+        where: {email: "peter.pan@mail.se"},
+        update: {},
+        create: {
+            email: "peter.pan@mail.se",
+            password: "secretpassword",
+        }
+    })
+}
