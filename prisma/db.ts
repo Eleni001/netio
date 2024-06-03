b71ce516111b1539e4513e9e42254757b6e3ca99
Nathalie Lysefeldt <lysefeldt@outlook.com>
2024-06-03 15:47:00 +0200

    change to env file with database direct url, scripts added in packagejson, removal of pool and added accelerate Co-authored-by: Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se> Co-authored-by: Tomoyo Alvåg <toal13@users.noreply.github.com>

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,10 +1,4 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-neonConfig.webSocketConstructor = WebSocket;
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-export const db = new PrismaClient({ adapter });
+export const db = new PrismaClient().$extends(withAccelerate());
