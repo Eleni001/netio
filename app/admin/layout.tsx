27dcc64c6edde9ca38d80ae6f7632f22e78f3c23
oscar.jigbring <oscar.jigbring@hotmail.se>
2024-06-04 16:13:54 +0200

    Fixed some responsive issues, fixed some UX-styling

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { Box, Flex, Link, Wrap, WrapItem } from '@chakra-ui/react';
+import { Box, Grid, GridItem, Link, Text } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import { FaRegCreditCard, FaRegEye } from 'react-icons/fa';
 import { GoPlus } from 'react-icons/go';
 
+import { usePathname } from 'next/navigation';
 import AddCategoryButton from '../components/AddCategoryModal';
 
 interface Props {
@@ -12,81 +13,96 @@ interface Props {
 }
 
 export default function Layout({ children }: Props) {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <>
-      <Flex padding={10} justify="center">
-        <Wrap spacing={4} justify="center">
-          <WrapItem>
-            <AddCategoryButton />
-          </WrapItem>
-          <WrapItem>
-            <Link href="/admin/product/new">
-              <Box
-                as="button"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                bg="gray.700"
-                color="white"
-                borderRadius="md"
-                padding="4"
-                _hover={{
-                  bg: 'gray.600',
-                  transform: 'translateY(-2px)',
-                  boxShadow: 'lg',
-                }}
-              >
-                <GoPlus />
-                Add Product
-              </Box>
-            </Link>
-          </WrapItem>
-          <WrapItem>
-            <Link href="/admin/orders">
-              <Box
-                as="button"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                bg="gray.700"
-                color="white"
-                borderRadius="md"
-                padding="4"
-                _hover={{
-                  bg: 'gray.600',
-                  transform: 'translateY(-2px)',
-                  boxShadow: 'lg',
-                }}
-              >
-                <FaRegEye />
-                Show all orders
-              </Box>
-            </Link>
-          </WrapItem>
-          <WrapItem>
-            <Link href="/admin/products">
-              <Box
-                as="button"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                bg="gray.700"
-                color="white"
-                borderRadius="md"
-                padding="4"
-                _hover={{
-                  bg: 'gray.600',
-                  transform: 'translateY(-2px)',
-                  boxShadow: 'lg',
-                }}
-              >
-                <FaRegCreditCard />
-                Show all products
-              </Box>
-            </Link>
-          </WrapItem>
-        </Wrap>
-      </Flex>
+      <Grid
+        templateColumns={{
+          base: '1fr',
+          sm: 'repeat(2, 1fr)',
+          md: 'repeat(4, 1fr)',
+        }}
+        gap={5}
+        padding={5}
+        justifyItems="center"
+      >
+        <GridItem width={'100%'}>
+          <AddCategoryButton />
+        </GridItem>
+        <GridItem width={'100%'}>
+          <Link href="/admin/product/new">
+            <Box
+              as="button"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              bg={pathname.includes('product/new') ? 'gray.500' : 'gray.800'}
+              color="white"
+              borderRadius="md"
+              padding="4"
+              gap={'2'}
+              width={'100%'}
+              _hover={{
+                bg: 'gray.600',
+                transform: 'translateY(-2px)',
+                boxShadow: 'lg',
+              }}
+            >
+              <GoPlus />
+              <Text>Add Product</Text>
+            </Box>
+          </Link>
+        </GridItem>
+        <GridItem width={'100%'}>
+          <Link href="/admin/orders">
+            <Box
+              as="button"
+              display="flex"
+              gap={'2'}
+              alignItems="center"
+              justifyContent="center"
+              bg={pathname.includes('admin/orders') ? 'gray.500' : 'gray.800'}
+              color="white"
+              width={'100%'}
+              borderRadius="md"
+              padding="4"
+              _hover={{
+                bg: 'gray.600',
+                transform: 'translateY(-2px)',
+                boxShadow: 'lg',
+              }}
+            >
+              <FaRegEye />
+              <Text>Show all orders</Text>
+            </Box>
+          </Link>
+        </GridItem>
+        <GridItem width={'100%'}>
+          <Link href="/admin/products">
+            <Box
+              as="button"
+              gap={'2'}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              bg={pathname.includes('admin/products') ? 'gray.500' : 'gray.800'}
+              width={'100%'}
+              color="white"
+              borderRadius="md"
+              padding="4"
+              _hover={{
+                bg: 'gray.600',
+                transform: 'translateY(-2px)',
+                boxShadow: 'lg',
+              }}
+            >
+              <FaRegCreditCard />
+              <Text>Show all products</Text>
+            </Box>
+          </Link>
+        </GridItem>
+      </Grid>
       {children}
     </>
   );
