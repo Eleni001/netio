53e6afe91f3d0029441e786a9ab72de7a78cb824
Nathalie Lysefeldt <lysefeldt@outlook.com>
2024-06-05 14:17:31 +0200

    created userOrderCard and userPage

---
+++ app/components/admin/UserOrderCard.tsx
@@ -0,0 +1,63 @@
+'use client';
+import { OrderWithInformation } from '@/app/types';
+import { Button, Td, Text, Tr } from '@chakra-ui/react';
+import Link from 'next/link';
+import { useState } from 'react';
+
+interface Props {
+  order: OrderWithInformation;
+}
+
+export default function UserOrderCard(props: Props) {
+  const [loading] = useState(false);
+
+  const formattedDate = new Date(props.order.createdAt).toLocaleString(
+    'sv-SE',
+    {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    },
+  );
+
+  const totalItems = props.order.orderRows.reduce(
+    (acc, row) => acc + row.quantity,
+    0,
+  );
+
+  return (
+    <Tr>
+      <Td>
+        {props.order.sentStatus === true ? (
+          <Text color="green">Sent</Text>
+        ) : (
+          <Text color="red">Not Sent</Text>
+        )}
+      </Td>
+      <Td>{props.order.id}</Td>
+      <Td>{formattedDate}</Td>
+      <Td>{props.order.user.name}</Td>
+      <Td>{props.order.shippingAddress.street}</Td>
+      <Td>{JSON.stringify(props.order.total)}kr</Td>
+      <Td>{totalItems}</Td>
+      <Td isNumeric>
+        <Link href={`/admin/orders/details/${props.order.id}`}>
+          <Button
+            bg="rgba(78, 199, 145, 1)"
+            color="white"
+            size="sm"
+            mr="3px"
+            _hover={{
+              transform: 'translateY(2px)',
+              boxShadow: 'lg',
+            }}
+          >
+            Details
+          </Button>
+        </Link>
+      </Td>
+    </Tr>
+  );
+}
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,3 +1,67 @@
-export default function UserPage() {
-  return <div>UserPage</div>;
+import { db } from '@/prisma/db';
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  Table,
+  TableContainer,
+  Tbody,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import UserOrderCard from '../components/admin/UserOrderCard';
+
+export default async function UserPage() {
+  const orders = await db.order.findMany({
+    include: {
+      user: true,
+      shippingAddress: true,
+      orderRows: { include: { product: true } },
+    },
+  });
+  return (
+    <Flex padding={10} flexDirection={'column'} gap={5}>
+      <Center>
+        <Heading fontSize={30}>All Orders</Heading>
+      </Center>
+      <Box overflowX="auto">
+        <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Order Status</Th>
+                <Th>Order Number</Th>
+                <Th>Created at</Th>
+                <Th>Name</Th>
+                <Th>Address</Th>
+                <Th>Total price</Th>
+                <Th>Total Items</Th>
+                <Th isNumeric>Button</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {orders.map((order) => (
+                <UserOrderCard key={order.id} order={order} />
+              ))}
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th>Order Status</Th>
+                <Th>Order Number</Th>
+                <Th>Created at</Th>
+                <Th>Name</Th>
+                <Th>Address</Th>
+                <Th>Total price</Th>
+                <Th>Total Items</Th>
+                <Th isNumeric>Button</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Flex>
+  );
 }
