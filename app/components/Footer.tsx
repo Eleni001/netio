6621cefcbfa29cab56833f189ff45f9e8bc154de
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-06-05 12:59:10 +0200

    pages rendered depending on the user and foot icons can be seen from admin pages

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Image, Square, Stack, Flex, Box } from '@chakra-ui/react';
+import { Box, Flex, Image, Square, Stack } from '@chakra-ui/react';
 import { MiddleFooter } from './MiddleFooter';
 
 function Footer() {
@@ -8,19 +8,19 @@ function Footer() {
       <Flex py="5%" bg="black" color="gray.500" justify="space-around">
         <Stack direction={'row'} spacing={2}>
           <Square size={{ base: '60px', md: '100px' }}>
-            <Image src="klarna.png" alt="klarna" />
+            <Image src="/klarna.png" alt="klarna" />
           </Square>
           <Square size={{ base: '60px', md: '100px' }}>
-            <Image src="visa.png" alt="visa" />
+            <Image src="/visa.png" alt="visa" />
           </Square>
           <Square size={{ base: '60px', md: '100px' }}>
-            <Image src="master.png" alt="mastercard" />
+            <Image src="/master.png" alt="mastercard" />
           </Square>
           <Square size={{ base: '60px', md: '100px' }}>
-            <Image src="american.png" alt="american express" />
+            <Image src="/american.png" alt="american express" />
           </Square>
           <Square size={{ base: '60px', md: '100px' }}>
-            <Image src="swish.png" alt="swish" />
+            <Image src="/swish.png" alt="swish" />
           </Square>
         </Stack>
       </Flex>
