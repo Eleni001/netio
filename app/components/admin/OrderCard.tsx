ff88dbfe03fca55f38db8ee409a1cedf3922f200
Tomoyo Alvåg <tomoyo.tomoyo.m@gmail.com>
2024-06-05 10:16:07 +0200

    Fixed display times and total items (#78)
    * Fixed display times and total items
    
    * delete duplicates icon for admin loggin

--- app/components/admin/OrderCard.tsx
+++ app/components/admin/OrderCard.tsx
@@ -57,6 +57,22 @@ export default function OrderCard(props: Props) {
     }
   };
 
+  const formattedDate = new Date(props.order.createdAt).toLocaleString(
+    'sv-SE',
+    {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    },
+  );
+
+  const totalItems = props.order.orderRows.reduce(
+    (acc, row) => acc + row.quantity,
+    0,
+  );
+
   return (
     <TableContainer>
       <Table variant="simple">
@@ -78,11 +94,11 @@ export default function OrderCard(props: Props) {
               <OrderSentButton order={props.order} />
             </Td>
             <Td>{props.order.id}</Td>
-            <Td>{JSON.stringify(props.order.createdAt)}</Td>
+            <Td>{formattedDate}</Td>
             <Td>{props.order.user.name}</Td>
             <Td>{props.order.shippingAddress.street}</Td>
             <Td>{JSON.stringify(props.order.total)}kr</Td>
-            <Td></Td>
+            <Td>{totalItems}</Td>
             <Td isNumeric>
               <Link href={`/admin/orders/details/${props.order.id}`}>
                 <Button
